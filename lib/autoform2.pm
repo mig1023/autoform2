c4e3ea818a374bf5f360fd4d6db25ff7164fc346
mig1023 <mig1023@yandex.ru>
2020-04-07 21:58:30 +0300

    token

--- lib/autoform2.pm
+++ lib/autoform2.pm
@@ -11,7 +11,11 @@ sub startup {
 
   my $r = $self->routes;
 
-  $r->get('/')->to('autoform#index');
+  $r->get('/')->to('autoform#new_token');
+  
+  $r->get('/token/:token')->to('autoform#token');
+  
+  $r->get('/data/:token')->to('autoform#data');
 }
 
 1;
--- lib/autoform2/Controller/Autoform.pm
+++ lib/autoform2/Controller/Autoform.pm
@@ -1,11 +1,57 @@
 package autoform2::Controller::Autoform;
 use Mojo::Base 'Mojolicious::Controller';
 
-sub index {
+sub new_token {
 
-  my $self = shift;
+	my $self = shift;
+	
+	my $new_token = int( rand( 10000 ) );
+	
+	$self->redirect_to( "/token/$new_token" );
+}
 
-  $self->render(msg => 'Autoform2 hello world');
+sub token {
+
+	my $self = shift;
+	
+	my $token = $self->param( "token" );
+
+	$self->render( token => $token );
+}
+
+sub data {
+
+	my $self = shift;
+	
+	my $token = $self->param( "token" );
+  
+	my $dummy_data = [
+		{
+			name => "token_field",
+			label => "token: $token",
+			type => "text",
+		},
+		{
+			name => "field1",
+			label => "dummy field 1",
+			type => "input",
+			val => "",
+		},
+		{
+			name => "field2",
+			label => "dummy checkbox 1",
+			type => "checkbox",
+			val => "",
+		},
+		{
+			name => "field3",
+			label => "dummy field 2",
+			type => "input",
+			val => "",
+		},
+	];
+
+	$self->render( json => $dummy_data );
 }
 
 1;
