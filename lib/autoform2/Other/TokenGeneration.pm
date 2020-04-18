9296ca1099472d129ab5478b63cf30541d7e1cb2
mig1023 <mig1023@yandex.ru>
2020-04-18 21:27:05 +0300

    lock -> transaction

--- lib/autoform2/Other/TokenGeneration.pm
+++ lib/autoform2/Other/TokenGeneration.pm
@@ -7,9 +7,7 @@ sub token_generation
 {
 	my $self = shift;
 
-	my ( $token_existing, $token_existed_before, $token ) = ( 1, 0, 't' );
-
-	$self->db->query( "LOCK TABLES AutoToken WRITE, AutoToken_expired READ" );
+	my $transaction = $self->db->begin;
 	
 	my $appid = $self->db->query( "
 		INSERT INTO AutoToken (
@@ -21,29 +19,18 @@ sub token_generation
 	my $appidcode = "-$appid-";
 
 	my @alph = split( //, '0123456789abcdefghigklmnopqrstuvwxyz' );
-
-	do {
-		$token = 't';
-		
-		$token .= $alph[ int( rand( 36 ) ) ] for ( 1..63 );
 	
-		substr( $token, 10, length( $appidcode ) ) = $appidcode;
-			
-		$token_existing = $self->db->query( "
-			SELECT ID FROM AutoToken WHERE Token = ?", $token
-		)->{ ID } || 0;
-		
-		$token_existed_before = $self->db->query( "
-			SELECT ID FROM AutoToken_expired WHERE Token = ?", $token
-		)->{ ID } || 0;
+	my $token = 't';
+	
+	$token .= $alph[ int( rand( 36 ) ) ] for ( 1..63 );
 
-	} while ( $token_existing || $token_existed_before );
+	substr( $token, 10, length( $appidcode ) ) = $appidcode;
 
 	$self->db->query( "
 		UPDATE AutoToken SET Token = ? WHERE ID = ?", $token, $appid
 	);
-
-	$self->db->query( "UNLOCK TABLES");	
+	
+	$transaction->commit;
 
 	return $token;
 }
