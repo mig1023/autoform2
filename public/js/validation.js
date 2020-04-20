e04eee4bfa51f51f7fa37a11d5554c2317339695
mig1023 <mig1023@yandex.ru>
2020-04-20 22:31:17 +0300

    full form validation

--- public/js/validation.js
+++ public/js/validation.js
@@ -1,56 +1,86 @@
 var validation = {
-	ok( input, check ) {
+
+	all_ok( form ) {
 		
-		var val = input.value.replace(/^\s+|\s+$/g, '');
+		var all_error = '';
 		
-		if ( ( val == '' ) || ( check == '' ) ) {
-			return true;
-		}
-		
-		if ( input.type == 'text' ) {
+		form.elements.forEach(function(item) {
 			
-			if ( /z/.test( check ) && ( val == '' ) ) {
+			var next_error = error( item.val, item.check );
+			
+			if ( !isEmpty( next_error ) )
+				all_error += item.name + ': ' + next_error + '\n';
+		});
+		
+		if ( isEmpty( all_error ) )
+			return true;
+		else {
+			alert( all_error );
+			return false;
+		}
+	},
+	
+	ok( input, check ) {
+		return ( isEmpty( error( input.value, check ) ) ? true : false );
+	},
+	
 
-				return false;
-			}
+}
 
-			if ( /D/.test( check ) ) {
+function error( value, check ) {
+	
+	if ( ( value == '' ) && ( check == '' ) )
+		return '';
+		
+	if ( value == undefined )
+		value = '';
+	
+	var val = value.replace(/^\s+|\s+$/g, '');
+ 	
+	if ( /z/.test( check ) && ( val == '' ) ) {
 
-				var date_reg = new RegExp( check.replace( /(z|D)/g, '' ) );
+		return 'Cant be empty';
+	}
 
-				if ( !date_reg.test( val ) && ! (val == '') ) {
-					
-					return false;
-				}
-			}
-			else {
-				var regexp = '';
+	if ( /D/.test( check ) ) {
 
-				if ( /W/.test( check ) ) {
-					regexp += 'A-Za-z';
-				}
-				if ( /¨/.test( check ) ) {
-					regexp += 'À-ß¨à-ÿ¸';
-				}
-				if ( /N/.test( check ) ) {
-					regexp += '0-9';
-				}
+		var date_reg = new RegExp( check.replace( /(z|D)/g, '' ) );
 
-				var regexp_add = check.replace( /(z|W|¨|N)/g, '' );
+		if ( !date_reg.test( val ) && ! (val == '') ) {
+			
+			return 'Wrong date format';
+		}
+	}
+	else {
+		var regexp = '';
 
-				var input_reg = new RegExp( '[^' + regexp + regexp_add + ']' );
-
-				var reverse_reg = new RegExp( '[' + regexp + regexp_add + ']', "g" );
-
-				if ( input_reg.test( val ) && ( val != '' ) ) {
-
-					return false;
-				}
-			}
+		if ( /W/.test( check ) ) {
+			regexp += 'A-Za-z';
+		}
+		if ( /¨/.test( check ) ) {
+			regexp += 'À-ß¨à-ÿ¸';
+		}
+		if ( /N/.test( check ) ) {
+			regexp += '0-9';
 		}
 
-		return true;
-	},
+		var regexp_add = check.replace( /(z|W|¨|N)/g, '' );
+
+		var input_reg = new RegExp( '[^' + regexp + regexp_add + ']' );
+
+		var reverse_reg = new RegExp( '[' + regexp + regexp_add + ']', "g" );
+
+		if ( input_reg.test( val ) && ( val != '' ) ) {
+
+			return 'Wrong symbols: ' + reverse_reg;
+		}
+	}
+		
+	return '';
+};
+
+function isEmpty( str ) {
+	return ( str.trim() == '' ? true : false );
 }
 
 export default validation;
